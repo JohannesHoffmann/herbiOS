09a4bf98a6c33e31b03568e1327793a4262bb04c
Johannes Hoffmann <hoppe@juhujuhu.de>
2021-08-30 20:58:17 +0200

    adds weather forecast and improves configuration

--- api/src/aroundMe/AroundMeConfig.ts
+++ api/src/aroundMe/AroundMeConfig.ts
@@ -1,10 +1,12 @@
-import { CurrentResponse } from "openweathermap-ts/dist/types";
+import { CurrentResponse, ThreeHourResponse } from "openweathermap-ts/dist/types";
 import JsonConfig from "../api/file/JsonConfig";
 
 export interface  IWeatherCurrent extends CurrentResponse {}
+export interface  IWeatherForecast extends ThreeHourResponse{}
 
 export interface IAroundMeConfig {
     currentWeather?: IWeatherCurrent;
+    forecastWeather?: IWeatherForecast;
 }
 
 export default class AroundMeConfig extends JsonConfig<IAroundMeConfig> {
