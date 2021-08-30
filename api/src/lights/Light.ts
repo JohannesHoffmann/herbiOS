ec0ebb7c4bcd4419fe945e8e51eef3faed499c0d
Johannes Hoffmann <hoppe@juhujuhu.de>
2021-08-30 21:00:40 +0200

    changes light configuration

--- api/src/lights/Light.ts
+++ api/src/lights/Light.ts
@@ -2,10 +2,10 @@ import SerialService from "../SerialService";
 
 class Light {
     name: string;
-    id: string;
+    id: Number;
     level: number = 0; // light level in percent
 
-    constructor(id: string, name: string) {
+    constructor(id: number, name: string) {
         this.id = id;
         this.name = name;
     }
@@ -30,9 +30,6 @@ class Light {
         }
 
         this.level = value;
-
-        // get the hardware id of the firmware from the lightId
-        let id = Number(this.id[this.id.length - 1]) - 1;
 
         // do serialport stuff here to send light level
         SerialService.sendFastCommand("setLight -light " + this.id + " -level " + Math.round(255/100 * this.level));
