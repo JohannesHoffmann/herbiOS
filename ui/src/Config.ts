9c5f9ff1034540d8bd0129c124fe328fd901229d
Johannes Hoffmann <hoppe@juhujuhu.de>
2021-08-30 21:01:56 +0200

    use localhost as api server when ui is also on localhost

--- ui/src/Config.ts
+++ ui/src/Config.ts
@@ -4,15 +4,11 @@ export interface IConfig {
     groundControl: string;
 }
 
+console.log( );
+
 const Config: IConfig = {
-    // host: "http://localhost:5555",
-    // tileServer:"http://localhost:5080",
-    // host: "http://192.168.180.25:5555",
-    // tileServer:"http://192.168.180.25:5080",
-    // host: "http://192.168.2.72:5555",
-    // tileServer:"http://192.168.2.72:5080",
-    host: "http://192.168.4.1:5555",
-    tileServer:"http://192.168.4.1:8088",
+    host: window.location.hostname === "localhost" ? "http://localhost:5555"  : "http://192.168.4.1:5555",
+    tileServer: window.location.hostname === "localhost" ? "http://localhost:8088"  : "http://192.168.4.1:8088",
     groundControl: "https://groundcontrol.juhujuhu.de",
 }
 
