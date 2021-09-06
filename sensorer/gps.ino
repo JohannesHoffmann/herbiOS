1dd66c46ed1ede5e2f453a4caa6e83d381ea26d8
Johannes Hoffmann <hoppe@juhujuhu.de>
2021-09-06 07:58:19 +0200

    removes unused serial initiation in gps.ino

--- sensorer/gps.ino
+++ sensorer/gps.ino
@@ -44,8 +44,8 @@ static void GPSloop()
 } 
 
 static void GPSsetup() {
-  Serial3.begin(9600);
-  gpsPort.begin( 9600 );
+  
+  gpsPort.begin(9600);
   gpsCommand = cli.addCommand("getPosition", gpsCallback);
 
   #if debug
@@ -58,6 +58,5 @@ void gpsCallback(cmd* c) {
     Command cmd(c); // Create wrapper object
 
     trace_all(Serial, gps, fix );
-    Serial.println("");
 }
 #endif
