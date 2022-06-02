0580a983c6563dac292bdd0c4cce1f4f48f6fb19
Johannes Hoffmann <hoppe@juhujuhu.de>
2022-06-02 11:14:58 +0200

    updated config

--- ui/src/Config.ts
+++ ui/src/Config.ts
@@ -1,15 +1,17 @@
 export interface IConfig {
     host: string;
+    socket: string;
     tileServer: string;
     groundControl: string;
 }
 
-console.log( );
+const host = "192.168.4.1";
 
 const Config: IConfig = {
-    host: window.location.hostname === "localhost" ? "http://localhost:5555"  : "http://192.168.4.1:5555",
-    tileServer: window.location.hostname === "localhost" ? "http://localhost:8088"  : "http://192.168.4.1:8088",
-    groundControl: "https://groundcontrol.juhujuhu.de",
+    host: window.location.hostname === "localhost" ? "/api"  : `https://${host}/api`,
+    socket: window.location.hostname === "localhost" ? "wss://localhost:8443"  : `wss://${host}:8443`,
+    tileServer: window.location.hostname === "localhost" ? "https://localhost:8444"  : `https://${host}:8444`,
+    groundControl: "https://groundcontrol.tld",
 }
 
 export default Config;
